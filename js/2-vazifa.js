ad4022c03bf4b28f60b233154b8ab17aacf4dc4a
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-01-24 21:43:23 +0500

    initial commit

---
+++ js/2-vazifa.js
@@ -0,0 +1,28 @@
+// let number = prompt("son kiriting!")
+
+// if(number < 10){
+//     console.log("bir xonali")
+// }
+// else if(number < 100 && number >= 10){
+//     console.log("ikki xonali")
+// }
+// else if(number < 1000 && number >= 100){
+//     console.log("uch xonali")
+// }
+// else if(number < 10000 && number >= 1000){
+//     console.log("to'rt xonali")
+// }
+
+
+
+// let num = prompt("son kiriting!")
+
+// if(num % 2 == 0){
+//     console.log("juft");
+// }
+// else{
+//     console.log("toq")
+// }
+
+
+
---
+++ js/app.js
@@ -0,0 +1,68 @@
+// let array1 = ["salom", "xayr" ,"Hoshimjon", "N49", "NT"]
+
+// for (i=0; i<array1.length;i++){
+//     if(array1[i] == "xayr"){
+//         alert(array1[i]);
+//     }
+// }
+
+
+
+// let array = [9, "Hoshimjon", true]
+
+// for (i=0; i<array.length; i++){
+//     if(typeof array[i] == "boolean"){
+//         alert(array[i])
+//     }
+// }
+
+
+
+// let number = [2, 7, 6, -4, -5, -9, -7, 4, 8]
+
+// for (i=0; i<number.length; i++){
+//     if(number[i] < 0){
+//         number[i] =Math.abs(number[i])
+//     }
+// }
+// console.log(number);
+
+
+
+// let num1 = prompt("son kiriting!")
+// let num2 = prompt("son kiriting!")
+// let num3 = prompt("son kiriting!")
+
+// alert((+num1 + +num2 + +num3)/3)
+
+
+
+// let number = prompt("3 xonali son kiriting!")
+
+// if((Math.floor(+number / 100))% 2 == 0){
+//     alert("birinchi raqami juft")
+// }
+// else{
+//     alert("birinchi raqami toq")
+// }
+
+
+
+// let elBall = prompt("Ball kiriting!")
+
+// if(elBall > 150){
+//     alert("yuqori ball")
+// }
+// else if(elBall <= 150){
+//     alert((+elBall / 1.5) +"%")
+// }
+
+
+
+// let number = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+// for (i=0; i<number.length; i++){
+//     if (number[i] % 2 == 0){
+//         console.log(number[i])
+//     }
+// }
